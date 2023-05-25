a1705a2cf676cdef272ffbe4a8fec9f8c9ae3afa
Arydiane <arydianejardim@gmail.com>
2023-05-25 20:45:12 -0300

    adicionando componente AbCampoTexto

---
+++ src/componentes/AbCampoTexto/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+
+const LabelEstilizada = styled.label`
+    color: #002F52;
+    display: block;
+    font-weight: 700;
+    font-size: 16px;
+    font-family: Arial, Helvetica, sans-serif;
+    margin-left: 16px;
+    margin-bottom: 8px;
+`
+const InputEstilizado = styled.input`
+    font-size: 16px;
+    line-height: 24px;
+    color: #002F52;
+    padding: 8px 24px;
+    border: 1px solid #002F52;
+    border-radius: 45px;
+    width: 100%;
+    box-sizing: border-box;
+    &:focus{
+        outline: none;
+    }
+`
+
+export interface AbCampoTextoProps {
+    label: string; 
+    value: string;
+    type?: 'text' | 'email' | 'password' | 'date';
+    onChange: (value: string) => void
+}
+
+export const AbCampoTexto = ({ label, value, onChange, type = 'text' } : AbCampoTextoProps) => {
+    return (
+        <div>
+            <LabelEstilizada>
+                {label}
+            </LabelEstilizada>
+            <InputEstilizado type={type}  value={value} onChange={(event) => onChange(event.target.value) }/>
+        </div>
+    )
+}
