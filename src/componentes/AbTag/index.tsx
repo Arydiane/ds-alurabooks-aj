c1d7d984d13476c4e1307c6833d3ad2224728645
Arydiane <arydianejardim@gmail.com>
2023-05-31 17:46:07 -0300

    atualiza comportamento do componente AbTag

--- src/componentes/AbTag/index.tsx
+++ src/componentes/AbTag/index.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 
-export interface AbTagProps {
+export interface AbTagStyleProps {
+    contexto?: 'primario' | 'secundario'
+}
+export interface AbTagProps extends AbTagStyleProps{
     texto: string
 }
 
-const TagEstilizada = styled.div`
-    padding: 24px 32px; 
+const TagEstilizada = styled.div<AbTagStyleProps>`
+    padding: 16px 24px; 
     color: #FFF; 
-    background: #EB9B00;
+    background: ${(props : AbTagStyleProps) => props.contexto === 'primario' ? '#EB9B00' : 'linear-gradient(97.54deg, #002F52 35.49%, #326589 165.37%)'};
     font-weight: 700; 
     font-size: 24px; 
     display: inline-block; 
     font-family: sans-serif;
 `
 
-export const AbTag = ({ texto } : AbTagProps) => {
+export const AbTag = ({ texto, contexto = 'primario' } : AbTagProps) => {
     return (
-        <TagEstilizada>
+        <TagEstilizada contexto={contexto}>
             {texto}
         </TagEstilizada>
     )
